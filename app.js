55914852de333ffd5555e61eb6fa7f27b9cc04db
sho-pc123 <syouteni123@gmail.com>
2024-04-20 19:21:54 +0900

    app.jsにhelloファイルを追加に伴う修正

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
 
 // エラーハンドラー
 app.use(function(req, res, next) {
