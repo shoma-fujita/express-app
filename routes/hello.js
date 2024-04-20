6268f5dedc986882ed30461072e07bc022afe0dc
sho-pc123 <syouteni123@gmail.com>
2024-04-20 19:26:08 +0900

    docの追加

--- routes/hello.js
+++ routes/hello.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// app.jsでアドレスと呼び出される設定は済みなので、ここの「/」は「/hello/ooo」の/ooo部分
 router.get('/', (req, res, next) => {
 var data = {
 title: 'Hello!',
