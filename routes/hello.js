46479b8013d366771e694a24b4f83eda2b02446b
sho-pc123 <syouteni123@gmail.com>
2024-04-20 19:29:52 +0900

    クエリから値を取得できるように修正

--- routes/hello.js
+++ routes/hello.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 // app.jsでアドレスと呼び出される設定は済みなので、ここの「/」は「/hello/ooo」の/ooo部分
 router.get('/', (req, res, next) => {
-var data = {
-title: 'Hello!',
-content: 'これは、サンプルのコンテンツです。<br>this is sample content.'
-};
-res.render('hello', data);
+  // reqオブジェクトのqueryからnameとmailの値を取り出している
+  var name = req.query.name;
+  var mail = req.query.mail;
+  var data = {
+    title: 'Hello!',
+    content: 'あなたの名前は、' + name + '。<br>' +
+      'メールアドレスは、' + mail + 'です。'
+  };
+  res.render('hello', data);
 });
 
 module.exports = router;
